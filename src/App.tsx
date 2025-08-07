69887941a5284bb49e47fde6ea7999cbcc1644da
AAYUSH garg <77825502+ashu-flty@users.noreply.github.com>
2025-08-07 13:47:32 +0530

    Add missing React imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
